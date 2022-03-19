b84a865d73f2867daa8723b7829c88fd7a139966
Nikita Bushuev <mrbushik1@gmail.com>
2022-03-19 20:02:05 +0300

    ready up Btn

--- src/modules/pageOrientation.js
+++ src/modules/pageOrientation.js
@@ -1,8 +1,10 @@
 const pageOrientation = () => {
     const menuItem = document.querySelector('.top-menu');
     const menuLink = menuItem.querySelectorAll('a');
+    const toUpBtn = document.querySelector('.up');
 
 
+    //header link
     menuLink[0].addEventListener('click', (e) => {
         e.preventDefault();
         window.scrollTo(0, 550);
@@ -15,6 +17,21 @@ const pageOrientation = () => {
         e.preventDefault();
         window.scrollTo(0, 4045);
     });
+
+
+    //up btn 
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset < 350) {
+            toUpBtn.style.display = 'none';
+        }
+        if (window.pageYOffset > 350) {
+            toUpBtn.style.display = 'block';
+        }
+        toUpBtn.addEventListener('click', () => {
+            window.scrollTo(0, 0);
+        });
+    });
+
 
 };
 export default pageOrientation;
