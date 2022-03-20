8ca545cec1fe39826fe1c720c7547bb17ff4f974
Nikita Bushuev <mrbushik1@gmail.com>
2022-03-20 10:46:12 +0300

    help btn

--- src/modules/callsModal.js
+++ src/modules/callsModal.js
@@ -3,6 +3,7 @@ const callsModal = () => {
     const modal = document.querySelector('.modal-callback ');
     const modalOverlay = document.querySelector('.modal-overlay');
     const close = document.querySelector('.modal-close');
+    const helpBtn = document.querySelector('.button-services')
 
 
     orderCallBtn[1].addEventListener('click', () => {
@@ -10,15 +11,22 @@ const callsModal = () => {
         modalOverlay.style.display = 'block';
 
     });
-    modal.addEventListener('click', () => {
+
+    close.addEventListener('click', () => {
         modal.style.display = 'none';
         modalOverlay.style.display = 'none';
     });
+
     modalOverlay.addEventListener('click', () => {
         modal.style.display = 'none';
         modalOverlay.style.display = 'none';
     });
 
+    //task 6 
+    helpBtn.addEventListener('click', () => {
+        modal.style.display = 'block';
+        modalOverlay.style.display = 'block';
+    });
 
 };
 export default callsModal;
