260ab66bcfa8e156da350c6a3c48fd58a3e16cc7
Nikita Bushuev <mrbushik1@gmail.com>
2022-03-22 21:27:44 +0300

    edit slider

--- src/modules/pageOrientation.js
+++ src/modules/pageOrientation.js
@@ -23,7 +23,6 @@ const pageOrientation = () => {
             const timeToUp = setInterval(() => {
                 let currentPagePosition = window.pageYOffset;
                 let position = currentPagePosition - 50;
-                let check = window.pageYOffset < target;
                 window.scrollTo(0, position);
                 if (currentPagePosition <= target) {
                     clearInterval(timeToUp);
--- src/modules/swiper.js
+++ src/modules/swiper.js
@@ -1,24 +1,64 @@
 const swiper = () => {
-    const swiper = new Swiper(".mySwiper", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
+    const slideItem = document.querySelectorAll('.slide__item');
+    const slideText = document.querySelectorAll('.table');
 
-    });
-    var swiper2 = new Swiper(".mySwiper2", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        navigation: {
-            nextEl: ".arrow-right",
-            prevEl: ".arrow-left",
-        },
-    });
+    let count = 1;
+
+    slideItem[0].style.display = 'block';
+    slideText[0].classList.add('active');
+
+    const timer = setInterval(() => {
+        count++;
+        if (count == 1) {
+            slideItem[2].style.display = 'none';
+            slideItem[0].style.display = 'block';
+        }
+        if (count == 2) {
+            slideItem[0].style.display = 'none';
+            slideItem[1].style.display = 'block';
+            slideText[1].classList.add('active');
+
+        }
+        if (count == 3) {
+            slideItem[1].style.display = 'none';
+            slideItem[2].style.display = 'block';
+            slideText[2].classList.add('active');
+            count = 0;
+        }
+    }, 3000);
+
+
+    if (window.screen.width < 576) {
+
+        const swiper2 = new Swiper(".mySwiper2", {
+
+            slidesPerView: 1,
+            spaceBetween: 30,
+
+            navigation: {
+                nextEl: ".arrow-right",
+                prevEl: ".arrow-left",
+            },
+
+        });
+
+    } else {
+        const swiper2 = new Swiper(".mySwiper2", {
+
+            slidesPerView: 3,
+            spaceBetween: 30,
+
+            navigation: {
+                nextEl: ".arrow-right",
+                prevEl: ".arrow-left",
+            },
+
+        });
+    }
+
+
+
+
+
 };
 export default swiper;
