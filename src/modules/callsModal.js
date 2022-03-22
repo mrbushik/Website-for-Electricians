8e84fdfdaadfba5fdc945f1a35dbab9ae35980d1
Nikita Bushuev <mrbushik1@gmail.com>
2022-03-22 14:27:36 +0300

    5 task is ready

--- src/modules/callsModal.js
+++ src/modules/callsModal.js
@@ -4,7 +4,7 @@ const callsModal = () => {
     const modalOverlay = document.querySelector('.modal-overlay');
     const close = document.querySelector('.modal-close');
     const helpBtn = document.querySelector('.button-services');
-
+    const repairItem = document.querySelectorAll('.repair__item');
 
     orderCallBtn[1].addEventListener('click', () => {
         modal.style.display = 'block';
@@ -20,6 +20,14 @@ const callsModal = () => {
     modalOverlay.addEventListener('click', () => {
         modal.style.display = 'none';
         modalOverlay.style.display = 'none';
+    });
+
+    //task 5
+    repairItem.forEach((item) => {
+        item.addEventListener('click', () => {
+            modal.style.display = 'block';
+            modalOverlay.style.display = 'block';
+        });
     });
 
     //task 6 
