f665c5d48dedbe2bc33ddc68dae64af925c50fde
Nikita Bushuev <mrbushik1@gmail.com>
2022-03-22 13:22:45 +0300

    ready first slider

--- src/modules/swiper.js
+++ src/modules/swiper.js
@@ -1,9 +1,16 @@
 const swiper = () => {
-    console.log('hello');
-    let swiper = new Swiper(".mySwiper", {
+    const swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
         pagination: {
             el: ".swiper-pagination",
+            clickable: true,
         },
+
     });
 };
 export default swiper;
