08be0a4f5c3f0581f870a04483ccc91d8cb4ba79
Nikita Bushuev <mrbushik1@gmail.com>
2022-03-22 23:24:59 +0300

    сделал слайдер

--- src/modules/swiper.js
+++ src/modules/swiper.js
@@ -13,17 +13,22 @@ const swiper = () => {
             slideItem[2].style.display = 'none';
             slideItem[0].style.display = 'block';
             slideText[0].classList.add('active');
+            slideText[2].classList.remove('active');
+
         }
         if (count == 2) {
             slideItem[0].style.display = 'none';
             slideItem[1].style.display = 'block';
             slideText[1].classList.add('active');
+            slideText[0].classList.remove('active');
 
         }
         if (count == 3) {
             slideItem[1].style.display = 'none';
             slideItem[2].style.display = 'block';
             slideText[2].classList.add('active');
+            slideText[1].classList.remove('active');
+
             count = 0;
         }
     }, 3000);
